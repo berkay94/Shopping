23c35a560db284a7dbbb93b78f885216f3866857
semvs <semvs@ayvansaray.edu.tr>
2019-02-14 14:13:22 +0300

    change

---
+++ Shoping.UI/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shopping.BLL.DTOs;
+using Shoping.UI.Models.view;
+using static Shopping.BLL.BussinessManager;
+namespace Shoping.UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel m)
+        {
+            try
+            {
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Shoping.UI/Models/view/CategoryModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Shopping.BLL.DTOs;
+namespace Shoping.UI.Models.view
+{
+    public class CategoryModel
+    {
+        public List<CategoriesDTOs> clist { get; set; }
+        public List<ProductsDTO> plist { get; set; }
+
+        public CategoryModel()
+        {
+            this.plist = new List<ProductsDTO>();
+        }
+    }
+}
